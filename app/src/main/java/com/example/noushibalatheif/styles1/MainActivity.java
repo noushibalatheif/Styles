8d29426eeb52db07774216efa0db32563cf886cb
noushibalatheif <noushibalatheif@gmail.com>
2017-07-05 04:03:32 -0700

    Assignment Five

---
+++ app/src/main/java/com/example/noushibalatheif/styles1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.noushibalatheif.styles1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+
+
+
+}
